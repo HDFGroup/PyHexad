6283072f4286337b010b3f5d08d1b2475ebf8861
gheber <leberecht@beingandti.me>
2014-10-13 10:23:17 -0500

    Added comments.

--- h5disp.py
+++ h5disp.py
@@ -1,5 +1,11 @@
+"""
+Display detailed information about HDF5 objects, including HDF5 attributes
+and their values.
+"""
 
 import automation
+import config
+from config import Limits
 import file_helpers
 from file_helpers import file_exists
 import functools
@@ -30,13 +36,13 @@ def h5disp(filename,location):
 #===============================================================================
 
     if not isinstance(filename, str):
-        raise TypeError, 'String expected.'
+        return "'filename' must be a string."
 
     if not isinstance(location, str):
-            raise TypeError, 'String expected.'
-
+        return "'location' must be a string."
+            
     if not file_exists(filename):
-        raise IOError, "Can't open file."
+        return "Can't open file."
 
     with h5py.File(filename, 'r') as f:
 
@@ -46,7 +52,7 @@ def h5disp(filename,location):
         obj = f[location]
         cls = f.get(location,getclass=True)
         
-        # generate the display
+        # generate the display - at the moment there are only two columns
 
         lines = []
 
@@ -59,7 +65,6 @@ def h5disp(filename,location):
             num_attr = len(obj.attrs)
             if num_attr > 0:
                 lines.append(('Number of attributes:', num_attr))
-
                 keys = obj.attrs.keys()
                 vals = obj.attrs.values()
                 for i in range(num_attr):
@@ -68,10 +73,9 @@ def h5disp(filename,location):
         # render object specific stuff
                 
         if cls == h5py.Group:
-
+            
             num_links = len(obj.keys())
-            lines.append(('Number of link:', num_links))
-
+            lines.append(('Number of links:', num_links))
             if num_links > 0:
                 lines.append(('Link names:', '\0'))
                 for i in range(num_links):
@@ -82,10 +86,14 @@ def h5disp(filename,location):
             lines.append(('Number of elements:', obj.size))
             lines.append(('Shape:', str(obj.shape)))
             lines.append(('Type:', str(obj.dtype)))
-            
+
         else:
             pass
 
+
+        # copy lines into Numpy array
+        # Is that really necessary?
+            
         dty = h5py.special_dtype(vlen=str)
         a = np.empty((len(lines)+1, 2), dtype=dty)
 
@@ -93,6 +101,8 @@ def h5disp(filename,location):
         for l in lines:
             a[row] = l
             row += 1
+            if row >= Limits.EXCEL_MAX_ROWS:
+                break
         
         # get the address of the calling cell using xlfCaller
         caller = pyxll.xlfCaller()
