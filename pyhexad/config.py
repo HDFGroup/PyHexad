258b7ea731c805e5ced5c37f11abef0d9360747b
gheber <leberecht@beingandti.me>
2014-11-12 14:46:32 -0600

    Eliminated h5xl.py.

--- pyhexad/config.py
+++ pyhexad/config.py
@@ -1,4 +1,7 @@
 
+from pyxll import xl_func
+
+
 class Limits(object):
 
     EXCEL_MAX_ROWS = 1048576
@@ -9,3 +12,40 @@ class Places(object):
 
     HDF5_HOME = 'C:\\Progra~1\\HDF_Group\\HDF5\\1.8.14'
     H52GIF = 'h52gifdll.exe'
+
+
+#==============================================================================
+
+
+@xl_func(": bool", volatile=True)
+def h5py_is_installed():
+    """returns True if h5py is installed"""
+    try:
+        import h5py
+        return True
+    except ImportError:
+        return False
+
+#==============================================================================
+
+
+@xl_func(": string", volatile=True)
+def h5py_version():
+    """returns the h5py version"""
+    try:
+        import h5py
+        return h5py.version.version
+    except ImportError:
+        return 'h5py is NOT installed.'
+
+#==============================================================================
+
+
+@xl_func(": string", volatile=True)
+def numpy_version():
+    """returns the numpy version"""
+    try:
+        import numpy
+        return numpy.version.version
+    except ImportError:
+        return 'Numpy is NOT installed.'
