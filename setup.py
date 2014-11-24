7d12665a24db77fd42f441b8115f9e9226a7a11b
Didrik Pinte <dpinte@enthought.com>
2014-11-24 10:55:44 +0100

    add author_email and url (needs to be filled in)

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name='pyhexad',
     version='0.0.1',
     author='HDFGroup',
+    author_email='',
+    url='',
     description='Excel wrapper for HDF5 using PyXLL',
     packages=setuptools.find_packages(),
 )
