8f788b58393f17a9e933dbb217611dca7d02d535
Gerd Heber <gheber@hdfgroup.org>
2014-11-24 07:30:44 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 setup(
     name='pyhexad',
     version='0.0.1',
-    author='HDFGroup',
-    author_email='',
-    url='',
-    description='Excel wrapper for HDF5 using PyXLL',
+    author='The HDF Group, Enthought',
+    author_email='gheber@hdfgroup.org, dpinte@enthought.com',
+    url='http://www.hdfgroup.org/pyhexad',
+    description='An HDF5 Excel add-in using PyXLL',
     packages=setuptools.find_packages(),
 )
