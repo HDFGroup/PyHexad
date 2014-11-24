632b8e925d370d42709ce1903966769db3fb455d
Didrik Pinte <dpinte@enthought.com>
2014-11-24 10:53:14 +0100

    basic setup.py for the pyhexad package

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='pyhexad',
+    version='0.0.1',
+    author='HDFGroup',
+    description='Excel wrapper for HDF5 using PyXLL',
+    packages=setuptools.find_packages(),
+)
