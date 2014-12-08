267273894f67784b943f08c8a13857c88c8cd43a
gheber <leberecht@beingandti.me>
2014-12-08 15:35:04 -0600

    Added copyright notice.

--- pyhexad/__init__.py
+++ pyhexad/__init__.py
@@ -1,3 +1,14 @@
+##############################################################################
+# Copyright by The HDF Group.                                                #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of PyHexad. The full PyHexad copyright notice, including #
+# terms governing use, modification, and redistribution, is contained in     #
+# the file COPYING, which can be found at the root of the source code        #
+# distribution tree.  If you do not have access to this file, you may        #
+# request a copy from help@hdfgroup.org.                                     #
+##############################################################################
+
 from .config           import Limits, h5py_is_installed, h5py_version, \
 		              numpy_version
 from .h5appendRows     import h5appendRows
--- pyhexad/shape_helpers.py
+++ pyhexad/shape_helpers.py
@@ -1,3 +1,13 @@
+##############################################################################
+# Copyright by The HDF Group.                                                #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of PyHexad. The full PyHexad copyright notice, including #
+# terms governing use, modification, and redistribution, is contained in     #
+# the file COPYING, which can be found at the root of the source code        #
+# distribution tree.  If you do not have access to this file, you may        #
+# request a copy from help@hdfgroup.org.                                     #
+##############################################################################
 
 import numpy as np
 
