662b08afd0d3d17e5615f60b2818c8499aadcc08
gheber <leberecht@beingandti.me>
2015-04-20 10:02:42 -0500

    Readthedocs doesnt like the alabaster theme.

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyHexad'
-copyright = u'2014, The HDF Group'
+copyright = u'2014, 2015, The HDF Group'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -108,7 +108,7 @@ rst_epilog = u"""
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
